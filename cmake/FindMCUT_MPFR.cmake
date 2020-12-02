c86098e5ddb0b4fe08ff6045b33c5e03d155d07b
chitalu <floyd.m.chitalu@gmail.com>
2020-12-02 13:04:47 +0100

    add copyright notice

--- cmake/FindMCUT_MPFR.cmake
+++ cmake/FindMCUT_MPFR.cmake
@@ -1,32 +1,23 @@
-# Originally copied from the MPPP project repository (12 Sept 2020):
-# https://raw.githubusercontent.com/bluescarni/mppp/master/cmake/Findmp%2B%2B_MPFR.cmake
+# Copyright (c) 2020-2021 CutDigital Ltd.
+# All rights reserved.
+# 
+# NOTE: This file is licensed under GPL-3.0-or-later (default). 
+# A commercial license can be purchased from CutDigital Ltd. 
+#  
+# License details:
+# 
+# (A)  GNU General Public License ("GPL"); a copy of which you should have 
+#      recieved with this file.
+# 	    - see also: <http://www.gnu.org/licenses/>
+# (B)  Commercial license.
+#      - email: contact@cut-digital.com
+# 
+# The commercial license options is for users that wish to use MCUT in 
+# their products for comercial purposes but do not wish to release their 
+# software products under the GPL license. 
+# 
+# Author(s)     : Floyd M. Chitalu
 
-# Copyright (c) 2006, Laurent Montel, <montel@kde.org>
-# Copyright (c) 2008-2020 Francesco Biscani, <bluescarni@gmail.com>
-
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-# 1. Redistributions of source code must retain the copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# ------------------------------------------------------------------------------------------
 
 if(MCUT_MPFR_INCLUDE_DIR AND MCUT_MPFR_LIBRARY)
     # Already in cache, be silent
