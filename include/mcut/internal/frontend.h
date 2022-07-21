14ebcdcc6aa893c4bf4746cb10bdf8f29389aaa4
chitalu <floyd.m.chitalu@gmail.com>
2022-07-21 13:23:51 +0200

    add frontend.h for refactor

---
+++ include/mcut/internal/frontend.h
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2021-2022 Floyd M. Chitalu.
+ * All rights reserved.
+ *
+ * NOTE: This file is licensed under GPL-3.0-or-later (default).
+ * A commercial license can be purchased from Floyd M. Chitalu.
+ *
+ * License details:
+ *
+ * (A)  GNU General Public License ("GPL"); a copy of which you should have
+ *      recieved with this file.
+ * 	    - see also: <http://www.gnu.org/licenses/>
+ * (B)  Commercial license.
+ *      - email: floyd.m.chitalu@gmail.com
+ *
+ * The commercial license options is for users that wish to use MCUT in
+ * their products for comercial purposes but do not wish to release their
+ * software products under the GPL license.
+ *
+ * Author(s)     : Floyd M. Chitalu
+ */
+
+/**
+ * @file mcut.h
+ * @author Floyd M. Chitalu
+ * @date 11 July 2022
+ *
+ * @brief API-function implementations.
+ *
+ * NOTE: This header file defines the pre- and post-cutting processing of mesh
+ * data, which includes any intermediate correctons/modifications to the user's
+ * input meshes like 'polygon partitioning'.
+ *
+ */
+
+#ifndef _FRONTEND_H_
+#define _FRONTEND_H_
+
+#include "mcut/mcut.h"
+
+namespace frontend {
+
+void create_context_impl(
+    McContext* pContext, McFlags flags);
+
+void debug_message_callback_impl(
+    McContext context,
+    pfn_mcDebugOutput_CALLBACK cb,
+    const void* userParam);
+
+void debug_message_control_impl(
+    McContext context,
+    McDebugSource source,
+    McDebugType type,
+    McDebugSeverity severity,
+    bool enabled);
+
+McResult dispatch_impl(
+    McContext context,
+    McFlags flags,
+    const void* pSrcMeshVertices,
+    const uint32_t* pSrcMeshFaceIndices,
+    const uint32_t* pSrcMeshFaceSizes,
+    uint32_t numSrcMeshVertices,
+    uint32_t numSrcMeshFaces,
+    const void* pCutMeshVertices,
+    const uint32_t* pCutMeshFaceIndices,
+    const uint32_t* pCutMeshFaceSizes,
+    uint32_t numCutMeshVertices,
+    uint32_t numCutMeshFaces);
+
+McResult get_info_impl(
+    const McContext context,
+    McFlags info,
+    uint64_t bytes,
+    void* pMem,
+    uint64_t* pNumBytes);
+
+McResult get_connected_components_impl(
+    const McContext context,
+    const McConnectedComponentType connectedComponentType,
+    const uint32_t numEntries,
+    McConnectedComponent* pConnComps,
+    uint32_t* numConnComps);
+
+McResult get_connected_component_data_impl(
+    const McContext context,
+    const McConnectedComponent connCompId,
+    McFlags flags,
+    uint64_t bytes,
+    void* pMem,
+    uint64_t* pNumBytes);
+
+McResult release_connected_components_impl(
+    const McContext context,
+    uint32_t numConnComps,
+    const McConnectedComponent* pConnComps);
+
+McResult release_context_impl(
+    McContext context);
+
+} // namespace frontend{
+
+#endif // #ifndef _FRONTEND_H_
