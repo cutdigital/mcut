f2028cea2c1943b1befc75c549f3d1a243b6d3b5
chitalu <floyd.m.chitalu@gmail.com>
2022-07-28 18:21:49 +0200

    minor reversion

--- source/kernel.cpp
+++ source/kernel.cpp
@@ -4292,23 +4292,23 @@ void dispatch(output_t& output, const input_t& input)
                 const vd_t ps_v1 = ps.vertex(ps_edge, 1);
 
                 MCUT_ASSERT((int)(ps_v0) < (int)ps_to_m0_vtx.size());
-                const vd_t m0_v0 = ps_to_m0_vtx[ps_v0];
+                const vd_t m0_v0 = ps_to_m0_vtx.at(ps_v0);
                 MCUT_ASSERT((int)(ps_v1) < (int)ps_to_m0_vtx.size());
-                const vd_t m0_v1 = ps_to_m0_vtx[ps_v1];
+                const vd_t m0_v1 = ps_to_m0_vtx.at(ps_v1);
 
-               // std::vector<vd_t> vertices_on_ps_edge = { ps_v0, ps_v1 };
+                std::vector<vd_t> vertices_on_ps_edge = { ps_v0, ps_v1 };
                 std::unordered_map<ed_t, std::vector<vd_t>>::const_iterator ps_intersecting_edges_iter = ps_intersecting_edges.find(ps_edge);
-                //if (ps_intersecting_edges_iter != ps_intersecting_edges.cend()) {
-                //    vertices_on_ps_edge.insert(
-                //        vertices_on_ps_edge.end(),
-                //        ps_intersecting_edges_iter->second.cbegin(),
-                //        ps_intersecting_edges_iter->second.cend());
-                //}
+                if (ps_intersecting_edges_iter != ps_intersecting_edges.cend()) {
+                    vertices_on_ps_edge.insert(
+                        vertices_on_ps_edge.end(),
+                        ps_intersecting_edges_iter->second.cbegin(),
+                        ps_intersecting_edges_iter->second.cend());
+                }
 
-                if (ps_intersecting_edges_iter == ps_intersecting_edges.cend() /*vertices_on_ps_edge.size() == 2*/) {
+                if (vertices_on_ps_edge.size() == 2) {
                     // const hd_t h = m0.add_edge(vertices_on_ps_edge.back(), vertices_on_ps_edge.front());
                     // MCUT_ASSERT(h != hmesh_t::null_halfedge());
-                    edges_LOCAL.push_back(std::make_pair(m0_v0, m0_v1));
+                    edges_LOCAL.push_back(std::make_pair(vertices_on_ps_edge.back(), vertices_on_ps_edge.front()));
                     const ed_t edge = ed_t((ed_t::index_type)(edges_LOCAL.size() - 1));
                     const hd_t h(edge * 2);
 
@@ -4332,11 +4332,11 @@ void dispatch(output_t& output, const input_t& input)
                     }
                 } else {
 
-                    MCUT_ASSERT( ps_intersecting_edges_iter->second.size() == 1);
+                    MCUT_ASSERT(vertices_on_ps_edge.size() == 3);
 
-                    const vd_t& first = m0_v0;
-                    const vd_t& second = m0_v1;
-                    const vd_t& third = ps_intersecting_edges_iter->second[0];//vertices_on_ps_edge[2];
+                    const vd_t first = vertices_on_ps_edge[0];
+                    const vd_t second = vertices_on_ps_edge[1];
+                    const vd_t third = vertices_on_ps_edge[2];
 
                     hd_t h0;
                     hd_t h1;
@@ -4582,7 +4582,7 @@ void dispatch(output_t& output, const input_t& input)
             }
         } else { // this is the more complex case where we add minimal set of non overlapping edges between 3 vertices
 
-            MCUT_ASSERT(ps_intersecting_edges_iter->second.size() == 1);
+            MCUT_ASSERT(ps_intersecting_edges_iter->second[0].size() == 1);
 
             const vd_t& first = ps_v0;//vertices_on_ps_edge[0];
             const vd_t& second =ps_v1;//vertices_on_ps_edge[1];
@@ -7747,7 +7747,7 @@ void dispatch(output_t& output, const input_t& input)
     // NOTE: not all will be CCW if we have floating patches (in this case winding could be flipped)
     int total_ccw_patch_count = 0;
     std::vector<bool> patch_poly_enqueued(m0_polygons.size(), false);
-    std::queue<int> flood_fill_queue; // for building patch using BFS
+
     do {
         ///////////////////////////////////////////////////////////////////////////
         // Associate cut-mesh polygons with patches of the graph
@@ -7803,8 +7803,7 @@ void dispatch(output_t& output, const input_t& input)
 
         std::vector<int>& patch = patches[graph_cur_patch_idx]; // patch_insertion.first->second; // polygons of patch
         patch.reserve(cs_face_count);
-        
-        MCUT_ASSERT(flood_fill_queue.size() == 0);
+        std::queue<int> flood_fill_queue; // for building patch using BFS
 
         flood_fill_queue.push(std::get<1>(graph_interior_ihalfedge_pool)); // first polygon
         patch_poly_enqueued[std::get<1>(graph_interior_ihalfedge_pool)] = true;
