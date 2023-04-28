048d5a80e0cdffafb237f6f1feb85860b818529f
chitalu <floyd.m.chitalu@gmail.com>
2023-04-28 19:15:49 +0200

    add cdt license file

--- include/mcut/internal/cdt/cdt.h
+++ include/mcut/internal/cdt/cdt.h
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 #ifndef _CONSTRAINED_DELAUNAY_TRIANGULATION_H_
 #define _CONSTRAINED_DELAUNAY_TRIANGULATION_H_
 
--- include/mcut/internal/cdt/triangulate.h
+++ include/mcut/internal/cdt/triangulate.h
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 #ifndef CDT_vW1vZ0lO8rS4gY4uI4fB
 #define CDT_vW1vZ0lO8rS4gY4uI4fB
 
